8e8fc7452ffe535b72ecee97e5ba80760ccc49a2
Husnain <Husnain.tayab@hotmail.com>
2025-08-27 18:23:28 +0500

    Rent Calculator

---
+++ Rent calculator.py
@@ -0,0 +1,10 @@
+
+
+rent = int(input("Enter Your Flat/Room Rent Please : "))
+food = int(input("Enter Your Food Expenses Please : "))
+electricity = int(input("Enter Your Total Unit of Electircity : "))
+charge = int(input("Enter the Each charge Price : "))
+person = int(input("Enter Number of persons :"))
+total_bill = electricity*charge
+total_expense = (rent+food+total_bill)/person
+print(f"Each person will pay : {total_expense} ")
