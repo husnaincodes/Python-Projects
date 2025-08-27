95c6a53f8c8775a94454b518dc1bfd1be21e0538
Husnain <Husnain.tayab@hotmail.com>
2025-08-27 19:38:04 +0500

    Quiz Game

---
+++ quiz game .py
@@ -0,0 +1,37 @@
+
+
+print("WELCOME TO MY COMPUTER GAME!")
+player = input("DO you want to play the game (yes/no):").lower()
+if player!="yes":
+    quit()
+print("okay! let's play the game ")
+
+answer = input("What is the full form of CPU ?:").lower()
+if answer== "central processing unit":
+    print("Congratulation! You are right")
+else:
+    print("You are wrong!")
+
+answer = input("What is the full form of DBMS? :").lower()
+if answer== "database management system":
+    print("Congratulation! You are right")
+else:
+    print("You are wrong!")
+
+answer = input("What does ICT stand for in the context of information management? :").lower()
+if answer== "information and communication technology":
+    print("Congratulation! You are right")
+else:
+    print("You are wrong!")
+
+answer = input("What is the full form of HTML in web design and development? :").lower()
+if answer== "hypertext markup language":
+    print("Congratulation! You are right")
+else:
+    print("You are wrong!")
+
+answer = input("What is the full form of PR in media and communication studies?:").lower()
+if answer== "public relations":
+    print("Congratulation! You are right")
+else:
+    print("You are wrong!")
