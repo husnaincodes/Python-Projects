f57768bce3cca140a45fd6ea5850495ef134ce02
Husnain <Husnain.tayab@hotmail.com>
2025-09-23 18:59:15 +0500

    Notification.py

---
+++ Notification.py
@@ -0,0 +1,33 @@
+
+import time
+import subprocess
+
+if __name__ == "__main__":
+    while True:
+        
+        subprocess.run([
+            "notify-send",
+            "𝑺𝒕𝒖𝒅𝒚 𝑻𝒊𝒎𝒆",
+            """Every hour I invest today is building the 4.0 CGPA I dream of tomorrow.
+Distractions fade, but my hard work will stay forever.
+I am capable, I am disciplined, and I will not stop until I reach the top.
+4.0 isn’t luck — it’s consistency. And today, I choose to give my best."""
+        ])
+        
+        
+        time.sleep(12960000)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#𝑺𝒕𝒖𝒅𝒚 𝑻𝒊𝒎𝒆
