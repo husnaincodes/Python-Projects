1263122501a6f15d8625f4defd699af9fa610251
Husnain <Husnain.tayab@hotmail.com>
2025-09-23 18:37:37 +0500

    Email sender

---
+++ Email sender.py
@@ -0,0 +1,32 @@
+
+import smtplib as s
+from getpass import getpass 
+user = input("Enter Your Email Please: ")
+
+password = getpass("Enter Your password please: ")
+
+email = s.SMTP("smtp.gmail.com",587)
+
+email.ehlo()
+
+email.starttls()
+
+email.login(user,password)
+
+subject =input("Please Enter Your Subject :")
+
+body = input("Enter Your message body please :")
+
+message = "Suject: {}\n\n{}".format(subject,body)
+
+listadd = ["Husnain.tayab@hotmail.com","husnaintayab6@gmail.com","sonytayyab306@gmail.com"]
+
+email.sendmail(user,listadd,message)
+
+print("SEND EMAIL SUCCESSFULLY!")
+
+email.quit()
+
+
+
+
