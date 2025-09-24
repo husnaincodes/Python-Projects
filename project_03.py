ddae32f132250cf5f950214da7fe77db8b9aeedb
Husnain <Husnain.tayab@hotmail.com>
2025-09-24 20:12:03 +0500

    QR Code

--- project_03.py
+++ project_03.py
@@ -1,9 +1,16 @@
 import qrcode
 data = input("Enter Your URL Please : ").strip()
+
 filename = input("Enter Your File Name : ").strip()
+
 qr = qrcode.QRCode(box_size=10,border=4)
+
 qr.add_data(data)
+
 qr.make(fit=True)
+
 image = qr.make_image(fill_color= "black",back_color = "white")
+
 image.save(filename)
+
 print(f"QR code save as {filename}")
