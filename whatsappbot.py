24c767d0bb835f1f2cc1615fdb3ecee5b6352a38
Husnain <Husnain.tayab@hotmail.com>
2025-10-16 01:18:41 +0500

    Simple whatsapp auto messenger

---
+++ whatsappbot.py
@@ -0,0 +1,8 @@
+import pywhatkit
+phone_number = (input("Enter the Phone number of receiver in (+92): "))
+message = input("Enter the message you want to sent: ")
+
+time = int(input("Enter the time in hours : "))
+minutes  = int(input("Enter the number in minutes(1,2,3,10): "))
+
+pywhatkit.sendwhatmsg(phone_number,message,time,minutes)
