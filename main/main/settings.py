a0cf898a0979ddb4448aedd5b890de7ea1b59078
Husnain <Husnain.tayab@hotmail.com>
2025-10-18 23:36:23 +0500

    django web framework

--- main/main/settings.py
+++ main/main/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-# add manually
+# Add manually
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
