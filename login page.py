d869b94b8d5676fb89f34788defd95ae4a789ebb
Husnain <Husnain.tayab@hotmail.com>
2025-10-31 23:34:30 +0500

    Flask login page

--- login page.py
+++ login page.py
@@ -25,13 +25,9 @@ def login():
 
     return    """
             <h2>Login page</h2>
-
             <form method = "POST">
-
             Username :<input type= "text" name = "username"><br>
-
             Password :<input type = "password" name = "password"><br>
-
             <input  type ="submit" value = "Login" >
             </form>
 
