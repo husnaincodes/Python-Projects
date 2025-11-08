c0cc4279b4684cc8d75336fbbb95ec337259f158
Husnain <Husnain.tayab@hotmail.com>
2025-11-08 23:01:06 +0500

    Product Inventory Manager

---
+++ Product Inventory Manager.py
@@ -0,0 +1,7 @@
+products = [("Apple", 100), ("Banana", 60), ("Apple", 80), ("Orange", 120)]
+inventory = {}
+
+for item, qty in products:
+    inventory[item] = inventory.get(item, 0) + qty
+
+print(inventory)
