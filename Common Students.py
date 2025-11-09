a004ccbca5e3321fc963b5349b468afb34f2e4cc
Husnain <Husnain.tayab@hotmail.com>
2025-11-09 21:14:50 +0500

    Common Student

---
+++ Common Students.py
@@ -0,0 +1,6 @@
+math = {"Ali", "Sara", "Bilal", "Hina"}
+science = {"Hina", "Zara", "Ali", "Umer"}
+
+print("Enrolled in both subjects:", math & science)
+print("Only in Math:", math - science)
+print("Only in Science:", science - math)
