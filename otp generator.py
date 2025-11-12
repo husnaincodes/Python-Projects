f3ca260ad9e784e844a04e427a298d9f4c324e5b
Husnain <Husnain.tayab@hotmail.com>
2025-11-12 22:21:58 +0500

    OTP Generator

--- otp generator.py
+++ otp generator.py
@@ -2,6 +2,7 @@
 import random
 def otp_generator(length = 4):
     if length<0:
+        
         raise ValueError("Otp lenght must be postive")
     otp = "".join([str(random.randint(0,9)) for i in range(length)])
     return otp
