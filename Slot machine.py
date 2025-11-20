05ee734c9f160f3af0b4478085b2029292ef9a55
Husnain <Husnain.tayab@hotmail.com>
2025-11-20 20:28:02 +0500

    Slot machine

---
+++ Slot machine.py
@@ -0,0 +1,86 @@
+import random
+MAX_LINES = 3
+MAX_BET = 100
+MIN_BET = 1
+ROWS = 3
+COLS = 3
+symbol_count = {
+    "A":2,
+    "B":4,
+    "C":6,
+    "D":8
+}
+def get_slot_machine_spin(rows,cols,symbols):
+    all_symbols=[]
+    for symbol , symbol_count in symbols.items():
+        for _ in range(symbol_count):
+            all_symbols.append(symbol)
+    colums = []
+    for col in range(cols):
+        column = []
+        current_symbols  =all_symbols
+        for  row in range (rows):
+            value =random.choice(all_symbols)
+            current_symbols.remove(value)
+
+
+
+def deposit():
+    while True:
+        amount = input("What would you like deposit? $: ")
+        if amount.isdigit():
+            amount = int(amount)
+            if amount>0:
+                break
+            else:
+                print("Amount must be greater than 0")
+        else:
+            print("Enter only digit")
+    return amount
+
+def get_the_lines():
+
+    while True:
+        lines = input(f"Enter the number of lines you bet on (1-{str(MAX_LINES)}): ")
+        if lines.isdigit():
+            lines = int(lines)
+            if 1<=lines<=MAX_LINES:
+                break
+            else:
+                print("Enter a valid  number of lines")
+        else:
+            print("Enter only digit")
+    return lines
+
+def get_bet():
+    while True:
+        amount = input("what would you bet? :$")
+        if amount.isdigit():
+            amount = int(amount)
+            if MIN_BET<=amount<=MAX_BET:
+                break
+            else:
+                print(f"Amount must be between ${MIN_BET}-${MAX_BET}")
+        else:
+            print("Enter only digit")
+    return amount
+
+
+
+
+
+def main():
+    balance = deposit()
+    lines = get_the_lines()
+    while True:
+        
+        bet = get_bet()
+        total_bet= bet*lines
+        if total_bet>balance:
+            print(f"You do not have enough to bet that amount , your current balance is: {balance}")
+        else:
+            break
+
+    print(f"You are betting $ {bet} on {lines} lines. Total bet is equal to : ${total_bet} ")
+    
+main()
