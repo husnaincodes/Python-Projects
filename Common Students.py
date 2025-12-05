efacdd676b724a631177ff43f97a441cd7b23a50
Husnain <Husnain.tayab@hotmail.com>
2025-12-05 20:41:21 +0500

    Django webframe work

--- Common Students.py
+++ Common Students.py
@@ -1,5 +1,5 @@
 math = {"Ali", "Sara", "Bilal", "Hina"}
-science = {"Hina", "Zara", "Ali", "Umer"}
+science = {"Hina", "Zara", "Ali", "Umer", "Ahmad"}
 
 print("Enrolled in both subjects:", math & science)
 print("Only in Math:", math - science)
