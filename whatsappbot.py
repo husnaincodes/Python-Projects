1c30308a587b9dcdae2c4791a148043db0fe7562
Husnain <Husnain.tayab@hotmail.com>
2025-12-21 22:57:57 +0500

    Whatsapp bot

--- whatsappbot.py
+++ whatsappbot.py
@@ -2,6 +2,7 @@ import pywhatkit
 phone_number = (input("Enter the Phone number of receiver in (+92): "))
 message = input("Enter the message you want to sent: ")
 
+
 time = int(input("Enter the time in hours : "))
 minutes  = int(input("Enter the number in minutes(1,2,3,10): "))
 
