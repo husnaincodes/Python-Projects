e02071dc888ca318631ef03a6928bdfb4fd77cfa
Husnain <Husnain.tayab@hotmail.com>
2026-01-30 21:42:03 +0500

    Youtube video downloader

---
+++ Youtube video downloader.py
@@ -0,0 +1,10 @@
+import yt_dlp
+def download_yt_video(url):
+    ydl_opts = {
+        "format" : "bestvideo[height<=1080]",
+        "noplaylist" :True,
+
+    }
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([url])
+        
